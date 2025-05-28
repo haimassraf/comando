383008dc2bbee5fb63775ec6ce8fd416083a6763
Haim Assraf <haimassraf@gmail.com>
2025-05-28 12:55:49 +0300

    f

--- Program.cs
+++ Program.cs
@@ -13,16 +13,13 @@ namespace comando
             Game game1 = new Game();
             SeaCommando newSeaComando = (SeaCommando)game1.CreateComandor("haim", "sea king", CommandoType.SeaCommando);
             AirCommando newAirCommando = (AirCommando)game1.CreateComandor("haim", "air king", CommandoType.AirCommando);
-            Weapon newWeapon = game1.CreateWeapon("ak47", "kalatchnikov", 5);
-            Enemy newEnemy = game1.CreateEnemy("blah blag");
+            Weapon weapon1 = game1.CreateWeapon("AK47", "KALASHNIKOV", 30);
+            Weapon weapon2 = game1.CreateWeapon("M16", "COLT", 29);
+            Enemy newEnemy = game1.CreateEnemy("blah blah");
             newSeaComando.Attack();
             newSeaComando.Sweaming();
             newAirCommando.parachuting();
             newEnemy.Yell();
-            for (int i = 0; i < 7; i++)
-            {
-                newWeapon.Shoot();
-            }
         }
     }
 }
--- SeaCommando.cs
+++ SeaCommando.cs
@@ -9,7 +9,7 @@ namespace comando
     class SeaCommando : Commando
     {
         public SeaCommando(string name, string nameCode) : base(name, nameCode) { }
-        public void Sweaming() => Console.WriteLine($"The commandor whith code name {this.CodeName} know sweaming");
-        public override void Attack() => Console.WriteLine($"Sea commandor with code name {this.CodeName} now attacking.");
+        public void Sweaming() => Console.WriteLine($"The commando whith code name {this.CodeName} know sweaming");
+        public override void Attack() => Console.WriteLine($"Sea commando with code name {this.CodeName} now attacking.");
     }
 }
